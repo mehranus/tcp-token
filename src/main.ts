7715114bbc1e90022db651bc33b5650afbcf2175
mehran feriduni <m3hr4nus@gmail.com>
2025-01-02 11:18:11 +0330

    fix bug

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.TCP,
       options: {
         host: "0.0.0.0",
-        port: +process.env.TOKEN_SERVICE_PORT,
+        port: 4002,
       },
     }
   );
